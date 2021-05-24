c96057168962cc1d243ff8e55e4f57a67481a1b1
tomas briones <tomasbriones@gmail.com>
2021-05-24 19:38:07 -0400

    creatin a new column age

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
   def index
   end
+  def create
+    User.create(email: params[:email])
+  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_24_010127) do
+ActiveRecord::Schema.define(version: 2021_05_24_232808) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "age"
   end
 
 end
