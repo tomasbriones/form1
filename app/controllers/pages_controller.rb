d7df6897e1efc94ad80e4e827f4ace25cb473aff
tomas briones <tomasbriones@gmail.com>
2021-05-25 00:31:11 -0400

    add boostrap to form 3.0

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   def index
   end
   def create
-    User.create(email: params[:email])
+    @user = User.create(email: params[:email], age: params[:age], comment: params[:comment])
   end
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_24_232808) do
+ActiveRecord::Schema.define(version: 2021_05_24_235347) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "age"
+    t.text "comment"
   end
 
 end
